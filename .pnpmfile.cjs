bfca83c8d79859d002e4c502653754e2947ed8ca
高阳 <yang.gao1@eeoa.com>
2024-06-05 14:13:22 +0800

    change pnpmfile

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,14 +2,11 @@ const fs = require('fs');
 const versionNode = Number(process.version.split('.')[0].substring(1));
 
 function readPackage(pkg, context) {
-    console.log('before',JSON.stringify(pkg))
-    console.log()
-    if (pkg.name === 'typescript') {
-        pkg.version = '4.9.5';
-        pkg.devDependencies['typescript'] = '4.9.5';
-    }
-    console.log('after',JSON.stringify(pkg))
-    return pkg;
+    const { dependencies, devDependencies, peerDependencies } = pkg
+    if (dependencies?.typescript) dependencies.typescript = '4.9.5'
+    if (devDependencies?.typescript) peerDependencies.typescript = '4.9.5'
+    if (peerDependencies?.typescript) peerDependencies.typescript = '4.9.5'
+    return pkg
 }
 
 module.exports = {
